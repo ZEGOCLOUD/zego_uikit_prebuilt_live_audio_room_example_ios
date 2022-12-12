4ae76b192f0c6511f3664285b4bf31571cc27376
careyxi <careyxi@zego.im>
2022-12-12 15:31:54 +0800

    add live audio room demo.

---
+++ ZegoUIKitPrebuiltLiveAudioRoomDemo/ZegoUIKitPrebuiltLiveAudioRoomDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZegoUIKitPrebuiltLiveAudioRoomDemo
+//
+//  Created by zego on 2022/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
