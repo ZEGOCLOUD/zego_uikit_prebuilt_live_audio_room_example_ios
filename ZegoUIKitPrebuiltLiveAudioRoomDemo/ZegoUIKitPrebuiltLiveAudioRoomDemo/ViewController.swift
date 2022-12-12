6d40a1d42716d28829b2c06d6cff50de734ea4ff
wukun <wukun@zego.im>
2022-12-12 16:33:31 +0800

    ZegoUIKitPrebuiltLiveAudioRoomDemo

--- ZegoUIKitPrebuiltLiveAudioRoomDemo/ZegoUIKitPrebuiltLiveAudioRoomDemo/ViewController.swift
+++ ZegoUIKitPrebuiltLiveAudioRoomDemo/ZegoUIKitPrebuiltLiveAudioRoomDemo/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import ZegoUIKitPrebuiltLiveAudioRoom
 
 class ViewController: UIViewController {
+    
+    let appID: UInt32 = <#AppID#>
+    let appSign: String = <#AppSign#>
+    var userID: String = String(format: "%d", arc4random() % 999999)
+    var userName: String = String(format: "n_%d", arc4random() % 999999)
+    var roomID: String = "99999"
 
+    @IBAction func startLiveAudio(_ sender: Any) {
+        let config: ZegoUIKitPrebuiltLiveAudioRoomConfig = ZegoUIKitPrebuiltLiveAudioRoomConfig.host()
+        let liveAudioVC: ZegoUIKitPrebuiltLiveAudioRoomVC = ZegoUIKitPrebuiltLiveAudioRoomVC(appID, appSign: appSign, userID: userID, userName: userName, roomID: roomID, config: config)
+        liveAudioVC.modalPresentationStyle = .fullScreen
+        self.present(liveAudioVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func joinLiveAudio(_ sender: Any) {
+        let config: ZegoUIKitPrebuiltLiveAudioRoomConfig = ZegoUIKitPrebuiltLiveAudioRoomConfig.audience()
+        let liveAudioVC: ZegoUIKitPrebuiltLiveAudioRoomVC = ZegoUIKitPrebuiltLiveAudioRoomVC(appID, appSign: appSign, userID: userID, userName: userName, roomID: roomID, config: config)
+        liveAudioVC.modalPresentationStyle = .fullScreen
+        self.present(liveAudioVC, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
 
 }
 
